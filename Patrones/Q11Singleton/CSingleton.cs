a7c1569ccef80de6ff26dfd782a1e47adc566277
Rodney <r.abadarocha@gmail.com>
2020-02-21 17:57:48 -0300

    we

---
+++ Patrones/Q11Singleton/CSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q11Singleton
+{
+    class CSingleton
+    {
+        private string nombre;
+        private int id;
+        private static CSingleton instanciado;
+
+        private CSingleton(string nombre, int id)
+        {
+            this.nombre = nombre;
+            this.id = id;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public int Id { get => id; set => id = value; }
+
+        static public CSingleton crearSingleton(string nombre, int id)
+        {
+            if (instanciado == null)
+            {
+                instanciado = new CSingleton(nombre, id);
+
+                return instanciado;
+            }
+            return instanciado;
+        }
+    }
+}
---
+++ Patrones/Q11Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q11Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSingleton objeto1 = CSingleton.crearSingleton("Rodney", 07729);
+            CSingleton objeto2 = CSingleton.crearSingleton("pedro", 02329);
+
+            Console.WriteLine(objeto1.Nombre);
+            Console.WriteLine(objeto2.Nombre);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
